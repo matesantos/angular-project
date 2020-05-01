944a3059d77fbf24f8234a0a12fe56efeb5c078f
Mateus <matesantos@hotmail.com>
2020-05-01 20:56:04 -0300

    Crud-Angular

---
+++ crud/frontend/src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+
+  propLegal :string = 'legal porra'
+
+  constructor() { }
+
+  ngOnInit() :void{
+  }
+  
+  fazerAlgo() :void{
+    console.log('Fazendo algo, nessa porra!')
+  }
+
+}
